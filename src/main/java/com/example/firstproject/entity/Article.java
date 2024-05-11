acfc484553bad52667a7be6b54ef2fbe1fbf09ee
Boyun Choi <boyun1625@naver.com>
2024-05-12 00:35:05 +0900

    Delete Article(), toStrihng() and Add @AllArgsConstructor, @ToString

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article{
     @Id
@@ -18,26 +22,26 @@ public class Article{
     @Column
     private String date;
 
-    // Article 생성자 추가
-    public Article(Long id, String title, String content, String date) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.date=date;
-    }
-
-    public Article() {
-
-    }
-
-    // toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", date='"+date+'\''+
-                '}';
-    }
+//    // Article 생성자 추가
+//    public Article(Long id, String title, String content, String date) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//        this.date=date;
+//    }
+//
+//    public Article() {
+//
+//    }
+//
+//    // toString() 메서드 추가
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                ", date='"+date+'\''+
+//                '}';
+//    }
 }
