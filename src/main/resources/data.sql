43b9f942890e019976366c5d2e0d09b7670d6b7a
최보윤 <boyun1625@naver.com>
2024-06-18 20:13:01 +0900

    더미 데이터 설정하기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content, date) VALUES (1, '가가가가', 1111, 0617);
+INSERT INTO article(id, title, content, date) VALUES (2, '나나나나', 2222, 0618);
+INSERT INTO article(id, title, content, date) VALUES (3, '다다다다', 3333, 0619);
