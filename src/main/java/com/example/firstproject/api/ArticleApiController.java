636326411ffa9f2fd81ebd361aaa62b5bf8bf767
최보윤 <boyun1625@naver.com>
2024-06-29 23:55:03 +0900

    단일 데이터 조회 기능 추가

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -4,6 +4,7 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,10 +14,16 @@ public class ArticleApiController {
     @Autowired
     private ArticleRepository articleRepository;
     // GET
-    @GetMapping("/api/articles")
+    @GetMapping("/api/articles/{id}")
     public List<Article> index(){
         return articleRepository.findAll();
     }
+
+    @GetMapping("api/articles/{id}")
+    public Article show(@PathVariable Long id){
+        return articleRepository.findById(id).orElse(null);
+    }
+
     // POST
     // PATCH
     // DELETE
