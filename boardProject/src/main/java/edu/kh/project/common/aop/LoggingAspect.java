e4d3094b6425e661d191fd20980bc90bcdd406a3
Y0-0N63 <boyun1625@naver.com>
2025-12-24 11:23:25 +0900

    메서드 실행 소요 시간 기록해서 로그로 출력하기

--- boardProject/src/main/java/edu/kh/project/common/aop/LoggingAspect.java
+++ boardProject/src/main/java/edu/kh/project/common/aop/LoggingAspect.java
@@ -1,6 +1,10 @@
 package edu.kh.project.common.aop;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -18,7 +22,7 @@ public class LoggingAspect {
 	
 	/* 컨트롤러 수행 전 로그 출력(클래스, 메서드, 요청 ip)
 	 * "execution(* edu.kh.project..*Controller*.*(..))"를 매번 작성하는 것 대신 PointcutBundle에 모아두고 필요할 때 가져오기 */
-	@Before("PointcutBundle.controllerPointCut")
+	@Before("PointcutBundle.controllerPointCut()")
 	public void beforeController(JoinPoint jp) {
 		// AOP가 적용된 클래스 이름 얻어오기
 		String className = jp.getTarget().getClass().getSimpleName();
@@ -48,12 +52,48 @@ public class LoggingAspect {
 		log.info(sb.toString());
 	}
 	
+	// @Around 사용 시 > 반환형은 무조건 Object
+	// @Around 메서드 종료 시 > proceed 반환값을 return해야만 함
+	// ProceedingJoinPoint : proceed 메서드를 제공하며, JoinPoint를 상속한 자식 객체로 @Around에서 사용 가능
+	// 	-> proceed() 메서드 호출 전후로 Before/After가 구분되어짐
 	/**
-	 * 접속자 IP 얻어오는 메서드
-	 *
-	 * @param request
-	 * @return ip
+	 * 서비스 수행 전/후로 동작하는 코드(클래스, 메서드, 파라미터, 비즈니스 로직의 실제 실행 시간)
+	 * @return
+	 * @throws Throwable 
 	 */
+	@Around("PointcutBundle.serviceImplPointCut()")
+	public Object aroundServiceImpl(ProceedingJoinPoint pjp) throws Throwable {
+		// Throwable : 예외 처리의 최상위 클래스 > Exception은 Throwable 자식 클래스 (Throwable - Exception/Error)
+		// + Exception(예외) : 개발자가 처리할 수 있는 문제, Error(오류) : 시스템 레벨의 심각한 문제(H/W의 문제 등)
+		
+		// AOP가 적용된 클래스 이름 얻어오기
+		String className = pjp.getTarget().getClass().getSimpleName();
+		
+		// 실행된 컨트롤러의 메서드명 얻어오기
+		String methodName = pjp.getSignature().getName();
+
+		log.info("-------------------------- {}.{} 서비스 호출 --------------------------", className, methodName);
+		
+		// 파라미터를 로그로 출력
+		log.info("Parameter : {}", Arrays.toString(pjp.getArgs()));
+		
+		// 서비스의 주요 비즈니스 로직 코드 실행 소요 시간을 기록하기
+		long startMs = System.currentTimeMillis();
+		
+		// ---------------------------- ^ Before // v After ----------------------------
+		Object obj = pjp.proceed(); // 전/후를 나누는 기준점이 됨
+		
+		long endMs = System.currentTimeMillis();
+		
+		log.info("RunningTime : {}ms", endMs-startMs);
+		log.info("----------------------------------------------------");
+		
+		return obj;
+	}
+	
+	/**	 접속자 IP 얻어오는 메서드
+	 * @param request
+	 * @return ip */
 	private String getRemoteAddr(HttpServletRequest request) {
 		// 클라이언트(사용자)의 실제 IP 주소를 찾아내기 위한 로직
 		String ip = null;
